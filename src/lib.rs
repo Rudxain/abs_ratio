21083a0278f13c4a341ce7e61c7c284b0a899ae4
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-04-10 04:18:41 -0400

    breaking-change: rename. add generic fn and more alts

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//#![no_std]
+#![no_std]
 #![warn(
 	unused,
 	future_incompatible,
@@ -17,10 +17,15 @@
 #![forbid(unsafe_code, clippy::exit)]
 #![allow(clippy::cargo_common_metadata)]
 
-#[allow(clippy::suboptimal_flops)]
+use core::{
+	num::NonZeroI128,
+	ops::{Div, Neg},
+};
+
+// `abs` method only exists in `std`
 #[must_use]
 #[inline]
-fn abs(x: f64) -> f64 {
+fn fabs(x: f64) -> f64 {
 	if x.is_sign_negative() {
 		-x
 	} else {
@@ -29,15 +34,25 @@ fn abs(x: f64) -> f64 {
 }
 
 #[must_use]
-#[inline]
-pub fn abs_ratio(x: f64, y: f64) -> f64 {
-	let [x, y] = [abs(x), abs(y)];
+pub fn abs_ratio_f(x: f64, y: f64) -> f64 {
+	let [x, y] = [fabs(x), fabs(y)];
 	x.max(y) / x.min(y)
 }
 
 #[must_use]
-#[inline]
-pub const fn abs_ratio_int(x: i128, y: i128) -> u128 {
+pub fn abs_ratio_nz(x: NonZeroI128, y: NonZeroI128) -> u128 {
+	let [x, y] = [u128::from(x.unsigned_abs()), u128::from(y.unsigned_abs())];
+	if x < y {
+		y / x
+	} else {
+		x / y
+	}
+}
+
+#[must_use]
+/// # Panics
+/// when either arg is 0
+pub const fn abs_ratio_i(x: i128, y: i128) -> u128 {
 	let [x, y] = [x.unsigned_abs(), y.unsigned_abs()];
 	if x < y {
 		y / x
@@ -47,8 +62,8 @@ pub const fn abs_ratio_int(x: i128, y: i128) -> u128 {
 }
 
 #[must_use]
-#[inline]
-pub const fn checked_abs_ratio_int(x: i128, y: i128) -> Option<u128> {
+/// Returns `None` when either arg is 0
+pub const fn checked_abs_ratio_i(x: i128, y: i128) -> Option<u128> {
 	let [x, y] = [x.unsigned_abs(), y.unsigned_abs()];
 	if x < y {
 		y.checked_div(x)
@@ -57,14 +72,23 @@ pub const fn checked_abs_ratio_int(x: i128, y: i128) -> Option<u128> {
 	}
 }
 
+pub fn abs_ratio<'a, T>(x: &'a T, y: &'a T) -> T
+where
+	&'a T: Ord + Neg<Output = &'a T> + Div<Output = T>,
+{
+	let (x, y) = (x.max(-x), y.max(-y)); //abs
+	x.max(y) / x.min(y)
+}
+
+
 #[cfg(test)]
 #[test]
-fn abs_test() {
+fn fabs_test() {
 	use core::cmp::Ordering::Equal;
 
-	assert!(abs(-0.0).total_cmp(&0.0) == Equal);
-	assert!(abs(-f64::NAN).total_cmp(&f64::NAN) == Equal);
-	assert!(abs(f64::NEG_INFINITY).total_cmp(&f64::INFINITY) == Equal);
+	assert!(fabs(-0.0).total_cmp(&0.0) == Equal);
+	assert!(fabs(-f64::NAN).total_cmp(&f64::NAN) == Equal);
+	assert!(fabs(f64::NEG_INFINITY).total_cmp(&f64::INFINITY) == Equal);
 }
 
 #[cfg(test)]
@@ -74,6 +98,6 @@ mod tests {
 
 	#[test]
 	fn it_works() {
-		assert_eq!(abs_ratio(2.0, 2.0), 1.0);
+		assert_eq!(abs_ratio_f(2.0, 2.0), 1.0);
 	}
 }
